9c6b83739221edd1dc9f2605e355ac892de4aace
Adi Mukherjee <ads.mukherjee@gmail.com>
2016-01-11 04:49:45 +0000

    It's messy, but it's working (for google trends). Socket.io communication between server and client, wget to download CSV, parsing to array and sending to client for rendering

---
+++ frontend/dist/js/app.js
@@ -0,0 +1,38 @@
+var socket = io.connect();
+socket.on('connect', function(data) {
+  socket.emit('join', 'hello from client ');
+  console.log('Client connected to server');
+});
+
+socket.on('messages', function(data) {
+  console.log(data);
+});
+
+socket.on('disconnect', function(data) {
+  console.log('Client disconnected from server');
+});
+
+$('#submit').click(function() {
+    var query = $('#search').val(),
+        data  = $('#data').val().toString(),
+        time  = $('#time').val().toString();
+    
+    if (query && data && time) {
+      var params = {
+        query : query,
+        data  : data,
+        date  : time
+      }
+      
+      console.log('Submitting params to server ', params);
+      socket.emit('search', params);
+    } else {
+      alert('Whoops! Looks like you forgot to enter something. Make sure Data Source, Time Scale and Search are all filled out!');
+    }
+    
+});
+
+socket.on('results', function(data) {
+  console.log(data);
+  drawVisualization(data);
+})
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var fs      = require('fs');
-var request = require('request');
-var mysql   = require('mysql');
-var express = require('express');
-var sys = require('sys');
-var exec = require('child_process').exec;
-var _ = require('underscore');
-
+var fs      = require('fs'),
+    request = require('request'),
+    mysql   = require('mysql'),
+    express = require('express'),
+    sys = require('sys'),
+    exec = require('child_process').exec,
+    path = require('path'),
+    sprintf = require("sprintf-js").sprintf,
+    cookie = require('cookies.txt');
 
 function puts(error, stdout, stderr) { console.log(stdout) };
 
@@ -78,13 +79,13 @@ function JSONify(data) {
     return json_obj;
 }
 
-function read_file(outfilename) {
+function read_file(outfilename, callback) {
 	fs.readFile(outfilename, 'utf8', function (err,data) {
 	  	if (err) {
 	    	return console.log(err);
 	  	}
         console.log("read data from "+outfilename);
-  		return data;
+  		callback(data);
   	});
 }
 
@@ -107,54 +108,167 @@ function get_csv_exec(query, date) {
 	if (date == "") {
 		for (var key in date_dict) {
 			var date = date_dict[key];
-			var csv_url ='https://www.google.co.uk/trends/trendsReport\?hl\=en-GB\&q\='+q+'\&geo\=GB\&date\='+d+'\&cmpt\=q\&tz\=Etc%2FGMT\&tz\=Etc%2FGMT\&content\=1\&export\=1';
+			var csv_url ='https://www.google.co.uk/trends/trendsReport\?hl\=en-GB\\\&q\='+q+'\\\&geo\=GB\\\&date\='+d+'\\\&cmpt\=q\\\&tz\=Etc%2FGMT\\\&tz\=Etc%2FGMT\\\&content\=1\\\&export\=1';
 			console.log(csv_url);
 			exec(command+' '+csv_url, puts);
 		}
 	} else {
         var d = encodeURIComponent(date.trim());
-        var csv_url = 'https://www.google.co.uk/trends/trendsReport\?hl\=en-GB\&q\='+q+'\&geo\=GB\&date\='+d+'\&cmpt\=q\&tz\=Etc%2FGMT\&tz\=Etc%2FGMT\&content\=1\&export\=1';
+        var csv_url = 'https://www.google.co.uk/trends/trendsReport\?hl\=en-GB\\\&q\='+q+'\\\&geo\=GB\\\&date\='+d+'\\\&cmpt\=q\\\&tz\=Etc%2FGMT\\\&tz\=Etc%2FGMT\\\&content\=1\\\&export\=1';
         console.log(encodeURIComponent(csv_url));
         exec(command+' '+csv_url, puts);
     }
 }
 
-function extract_csv_data(csv_filepath) {
-    fs.readFile(csv_filepath, 'utf8', function (err,data) {
-        if (err) {
-            return console.log(err);
-        } 
-        var values = data.match(/\d{4}-\d{2}-\d{2}-\d{2}:\d{2}\s\w{3},\d+/gm);
-        for (var i=0; i<values.length; i++) {
-            var line = values[i].split(',');
-            line[0] = line[0].match(/\d{4}-\d{2}-\d{2}-\d{2}:\d{2}/g);
-            values[i] = [line[0].toString(), line[1]];
-        }
+function extract_csv_data(data, options, callback) {
+    var values = data.match(/\d{4}-\d{2}-\d{2}-\d{2}:\d{2}\s\w{3},\d+/gm);
+    for (var i=0; i<values.length; i++) {
+        var line = values[i].split(',');
+        line[0] = line[0].match(/\d{4}-\d{2}-\d{2}-\d{2}:\d{2}/g);
+        values[i] = [line[0].toString(), parseFloat(line[1])];
+    }
+    values.unshift(['time', 'relative interest']); // add element to beginning of array
+    if (options.json) {
         values = JSON.stringify(values, null, 2);
-        console.log(values);
-    });
+    }
+    if (typeof callback ==  "function") {
+        callback(values);      
+    } else {    
+        return values;
+    }
 }
 
-function alternate_key_value(array) {
-    var object = {}
+
+// Adds leading 0's to all date params and concat's to last 2 characters to ensure 2 digit output
+function pretty_timestamp() {
+    var d = new Date()
+    var second = ('0' + d.getSeconds()).slice(-2),
+        minute = ('0' + d.getMinutes()).slice(-2),
+        hour = ('0' + d.getHours()).slice(-2),
+        day = ('0' + d.getDay()).slice(-2),
+        month = ('0' + d.getDay()+1).slice(-2),
+        year = d.getFullYear();
+    var formatted_date = sprintf('%s-%s-%s.%s:%s:%s', day, month, year, hour, minute, second)
+    return formatted_date
+}
+
+function wget(options, callback) {
+    var filename;
+    
+    function execute(cmd, callback) {
+        console.log('Executing: '+cmd);
+        exec(cmd, function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+              console.log('exec error: ' + error);
+            }
+            if (typeof callback == "function") {
+                read_file(filename, callback); // sends stdout to callback
+            } 
+        });
+    }
+    
+    if (options.query && options.date) {
+        var query = options.query.trim(), // remove leading and trailing whitespace
+            date = date_dict[options.date];
+        var URL = 'https://www.google.co.uk/trends/trendsReport\?hl\=en-GB\\&q\='+query+'\\&geo\=GB\\&date\='+date+'\\&cmpt\=q\\&tz\=Etc%2FGMT\\&tz\=Etc%2FGMT\\&content\=1\\&export\=1';
+        if (options.print) { // print to stdout (redirect to console)
+            var cmd = 'wget -x --load-cookies ./cookies.txt -O - -o /dev/null '+ URL;
+        } else { 
+            if (options.filename) {
+                var filename = options.filename;
+            } else { // set filename with datetime and search params
+                var d = new Date();
+                filename = sprintf("%s-%s.%s.csv", query, options.date, pretty_timestamp());
+            }
+            var cmd = 'wget -x --load-cookies ./cookies.txt -O "'+filename.toString() +'" '+ URL;
+        }
+        execute(cmd, callback);
+    }
+    
 }
 
 function main() {
 	check_command_line_arguments();
     change_to_file_directory();
-    extract_csv_data('shtest.csv');
+    // extract_csv_data('shtest.csv');
+    // wget('hello', date_dict['1h']);
+    // cookie.parse('./cookies.txt', function (jsonCookie) {
+    //     var URL = 'https://www.google.co.uk/trends/trendsReport?hl=en-GB&q=hello&geo=GB&date=&cmpt=q&tz=Etc%2FGMT&content=1&export=1';
+        
+    //     var options = {
+    //       url: URL,
+    //       headers: {
+    //         'Cookie' : jsonCookie
+    //       }
+    //     };
+        
+    //     function callback(error, response, body) {
+    //       if (!error && response.statusCode == 200) {
+    //         console.log(body);
+    //       }
+    //     }
+        
+    //     request(options, callback);
+    // });
 }
 
 main();
 
+
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-app.post('/', function(req, res) {
-    res.send('Goodbye World!');
-});
+app.use('/', express.static(__dirname + '/../frontend'));
 
-app.listen(process.env.PORT, process.env.IP);
+// for dev, will be removed in final
+app.use('/datavis', express.static(__dirname + '/../DataVisualisation'));
+
+io.on('connection', function(client) {
+    console.log('Client '+ client.id + ' connected.');
+    
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'hello from server');
+    });
+    
+    client.on('disconnect', function(data) {
+        console.log('Client '+ client.id + ' disconnected.');
+    });
+    
+    client.on('search', function(data) {
+        console.log('Search from client: ', data);
+        switch (data.data) {
+            case 'google':
+                console.log('Getting data from Google');
+                var options = {
+                    query   : data.query,
+                    date    : data.date,
+                    print   : false
+                };
+                wget(options, function(results) {
+                    var csv_data = extract_csv_data(results, { json : false })
+                    client.emit('results', csv_data)
+                });
+                break;
+            case 'twitter':
+                console.log('Getting data from Twitter');
+                var response = 0; /* get twitter data*/
+                break;
+            case 'instagram':
+                console.log('Getting data from Instagram');
+                var response = 0; /* get instagram data*/
+                break;
+        }
+    })
+})
+
+server.listen(process.env.PORT || 3000);

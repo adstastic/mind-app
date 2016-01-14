0d68e7304d09a077bdec3501c1cd6a121cfcfd13
Adi Mukherjee <ads.mukherjee@gmail.com>
2016-01-14 01:28:27 +0000

    fixed chart x-axis, added status labels

--- frontend/dist/js/app.js
+++ frontend/dist/js/app.js
@@ -1,13 +1,24 @@
+var secs = 0;
+
 $(document).ready(function () {
-  $('#graph').hide();
-  $('#tout').show();
+  google_loading()
+  setInterval(function() {
+      $('#twitter').find('.waiting').html('Waiting '+secs+'s');
+      secs++;
+    }, 1000);
 });
 
+
+/*global io*/
 var socket = io.connect();
 socket.on('connect', function(data) {
   socket.emit('join', 'hello from client ');
   console.log('Client connected to server');
-
+  
+  $('.status').removeClass('label-danger');
+  $('.status').removeClass('label-warning');
+  $('.status').addClass('label-success');
+  $('.status').html('Connected');
   
   var params_onload = {
         query : 'mind',
@@ -27,6 +38,7 @@ socket.on('disconnect', function(data) {
 });
 
 $('#submit').click(function() {
+    google_loading();
     var query = $('#search').val(),
         data  = $('#data').val(),
         time  = $('#time').val();
@@ -46,11 +58,11 @@ $('#submit').click(function() {
     
 });
 
+/*global drawVisualization*/
 socket.on('results', function(data) {
-  console.log(data.data);
-  drawVisualization(data.data, data.title);
-  $('#tout').hide();
-  $('#graph').show();
+  console.log(data);
+  drawVisualization(data);
+  google_ready();
 })
 
 var tweets = 0;
@@ -60,7 +72,33 @@ socket.on('twitter', function(data) {
   if(data.text.length > 0) {
     tweets++;
     $("#twitter-stream ul").prepend("<li class=\"list-group-item\">" + data.text+ "</li>");
-    $("#twitter-stream li").first().effect( "highlight", {color:"#194784"}, 3000 );
+    $("#twitter-stream li").first().effect( "highlight", {color:"#194784"}, 2000 );
+    var tweets = $('#twitter').find('.info').html();
+    $('#twitter').find('.info').html(++tweets);
+    secs = 0;
   }
 });
 
+socket.on('disconnect', function() {
+  $('.status').removeClass('label-success');
+  $('.status').addClass('label-danger');
+  var date = new Date();
+   $('#twitter').find('.status').html('Disconnected');
+  $('#google').find('.status').html('Disconnected since '+date.getHours()+':'+date.getMinutes());
+});
+
+function google_loading() {
+  $('#google').find('.status').removeClass('label-success')
+  $('#google').find('.status').addClass('label-warning')
+  $('#google').find('.status').html('Loading')
+  $('#graph').hide();
+  $('#tout').show();
+}
+
+function google_ready() {
+  $('#google').find('.status').removeClass('label-warning')
+  $('#google').find('.status').addClass('label-success')
+  $('#google').find('.status').html('Connected')
+  $('#tout').hide();
+  $('#graph').show();
+}
--- server/server.js
+++ server/server.js
@@ -89,24 +89,46 @@ function read_file(outfilename, callback) {
   	});
 }
 
+var date_syntax = {
+    week: 'YYYY-MM-DD - YYYY-MM-DD,VV',
+    day: 'YYYY-MM-DD,VV',
+    time: 'YYYY-MM-DD-HH:mm ZZZ,VV'
+};
+
+var regex_day_hour = /\d{4}-\d{2}-\d{2}-\d{2}:\d{2}\s\w{3},\d+/gm,
+    regex_month = /\d{4}-\d{2}-\d{2},\d+/gm,
+    regex_year = /\d{4}-\d{2}-\d{2}\s-\s\d{4}-\d{2}-\d{2},\d+/gm;
+
 var date_dict = {
-    'Last Year'    : "today+12-m",
-    '3m'     : "today+3-m",
-    'Last Month'     : "today+1-m",
-    'Last Week'     : "now+7-d",
-    'Last Day'     : "now+1-d",
-    '4h'     : "now+4-H",
-    'Last Hour'     : "now+1-H",
-    'ALl Time'     : ""
+    'Last Hour' : ["now+1-H", regex_day_hour],
+    '4h' : ["now+4-H", regex_day_hour],
+    'Last Day' : ["now+1-d", regex_day_hour],
+    'Last Week' : ["now+7-d", regex_day_hour],
+    'Last Month' : ["today+1-m", regex_month],
+    '3m' : ["today+3-m", regex_month],
+    'Last Year' : ["today+12-m", regex_year],
+    'All Time' : ["", regex_year]
 };  
 
 function extract_csv_data(data, options, callback) {
-    var values = data.match(/\d{4}-\d{2}-\d{2}-\d{2}:\d{2}\s\w{3},\d+/gm);
-    // if (values) {
+    var regex = date_dict[options.date][1]; 
+    var values = data.match(regex);
+    try {
         for (var i=0; i<values.length; i++) {
-            var line = values[i].split(',');
-            line[0] = line[0].match(/\d{4}-\d{2}-\d{2}-\d{2}:\d{2}/g);
-            values[i] = [line[0].toString(), parseFloat(line[1])];
+            var line = values[i].split(/[\s,:-]+/);
+            var year    = line[0],
+                month   = line[1]-1,
+                day     = line[2];
+            var date = new Date(year, month, day);
+            console.log(date)
+            if (line[3].length == 2) {
+                date.setHours(line[3]); 
+                date.setMinutes(line[4]);
+            }
+            
+            var value = line[line.length-1];
+            
+            values[i] = [date.toISOString(), parseFloat(value)];
         }
         values.unshift(['time', 'relative interest']); // add element to beginning of array
         if (options.json) {
@@ -114,12 +136,12 @@ function extract_csv_data(data, options, callback) {
         }
         if (typeof callback ==  "function") {
             callback(values);      
-        } else {    
-            return values;
-        }
-    // }
+        } else  return values;
+    }
+    catch(e) {
+        console.error("extract_csv_data:",e.message);
+    }
 }
-
 
 // Adds leading 0's to all date params and concat's to last 2 characters to ensure 2 digit output
 function pretty_timestamp() {
@@ -128,7 +150,7 @@ function pretty_timestamp() {
         minute = ('0' + d.getMinutes()).slice(-2),
         hour = ('0' + d.getHours()).slice(-2),
         day = ('0' + d.getDay()).slice(-2),
-        month = ('0' + d.getDay()+1).slice(-2),
+        month = ('0' + d.getDay()+1).slice(-2), // js months are indexed from 0
         year = d.getFullYear();
     var formatted_date = sprintf('%s-%s-%s.%s:%s:%s', day, month, year, hour, minute, second);
     return formatted_date;
@@ -154,7 +176,7 @@ function wget(options, callback) {
     
     if (options.query && options.date) {
         var query = options.query.trim(), // remove leading and trailing whitespace
-            date = date_dict[options.date];
+            date = date_dict[options.date][0];
         var URL = 'https://www.google.co.uk/trends/trendsReport\?hl\=en-GB\\&q\='+query+'\\&geo\=GB\\&date\='+date+'\\&cmpt\=q\\&tz\=Etc%2FGMT\\&tz\=Etc%2FGMT\\&content\=1\\&export\=1';
         if (options.print) { // print to stdout (redirect to console)
             var cmd = 'wget -x --load-cookies ./cookies.txt -O - -o /dev/null '+ URL;
@@ -171,18 +193,6 @@ function wget(options, callback) {
     
 }
 
-function main() {
-	check_command_line_arguments();
-    change_to_file_directory();
-}
-
-main();
-
-// change the keys and secrets to environment variables later for security, set using command line when node instantiated or using export
-
-
-
-
 function search(string, keywords) {
     var found;
     for (var i = 0; i < keywords.length; i++) {
@@ -196,142 +206,139 @@ function search(string, keywords) {
     return found;
 }   
 
-(function () {
-    
-    var app = express();
-    var server = require('http').createServer(app);
-    var io = require('socket.io')(server);
-    
-    var twitter = new Twit({
-        consumer_key : 'kirRWQYmrpOB45THZFXSICM2u',
-        consumer_secret : '1m2q5Q0tAPInrjGrvPQ1manCo27oAXWMdOcMaBFNA8iubjAini',
-        access_token : '33188875-5YWOXnLbgSRl8Tsfkjjqm2ao5uA0OuXQY8vg2tsbo',
-        access_token_secret : 'tqSIAHX6X9b8RnARVYahNtod3AWvf414dOPr57zqFx5aX'
-    });
-    
-    twitter.get('search/tweets', 
-        {
-            q : '#mentalhealth',
-            since : 2007-01-01
-        },
-        function (twitter_err, data, response) {
-            var subset = data.statuses;
-            if (twitter_err) throw twitter_err;
-        // 	fs.writeFile('./twitter_test.json', JSON.stringify(data.statuses.slice(0,2), null, 4), function(fs_err) {
-        // 	    if (fs_err) throw fs_err;
-        //       	console.log("Saved JSON string");
-        //   	});
-          	for (var status in subset) {
-          	    var hashtags = subset[status].entities.hashtags;
-          	    for (var hashtag in hashtags) {
-          	        console.log(hashtags[hashtag].text);
-          	    }
-          	}
+function process_tweet(tweet, search_array) {
+    console.time('process_tweet');
+    if (search(tweet.text.toLowerCase(), search_array)) {
+        var location = ((tweet.place.country) ? tweet.place.country : tweet.user.location);
+        if (tweet.entities.hashtags) {
+            for (var hashtag in tweet.entities.hashtags) {
+                console.log(hashtag);
+            }
         }
-    );
-    
-    var search_terms = "mindcharity,mentalhealth,Abuse,Addiction,dependency,Advocacy,Aftercare,Anger,Antidepressants,Antidepressants,Antipsychotics,Anxiety,panic attacks,Arts therapies,Benefits,Bereavement,Bipolar disorder,Body dysmorphic disorder,Borderline personality disorder,BPD,Carers,coping,Clinical Negligence,Cognitive behavioural therapy,CBT,Community care,aftercare,mental health,social care,Complementary therapy,alternative therapy,Consent to treatment,CRHT,Crisis services,Debt and mental health,Depression,Dialectical behaviour therapy,Disability discrimination,Discharge from hospital,Discrimination at work,Dissociative disorders,Driving,Drugs - street drugs & alcohol,Eating problems,Ecotherapy,Electroconvulsive therapy,Financial help,Hearing voices,Hoarding,Holidays and respite care,Housing,Human Rights Act 1998,Hypomania and mania,IMHAs (England),IMHAs (Wales),Insurance cover and mental health,Learning disability support,LGBT mental health,Lithium and other mood stabilisers,Loneliness,Medication,Medication - drugs A-Z,Medication - stopping or coming off,Mental Capacity Act 2005,Mental Health Act 1983,Mental health and the courts,Mental health and the police,Mental health problems,Mindfulness,Money and mental health,Nearest relative,Neurosurgery for mental disorder,Obsessive-compulsive disorder,ocd,Online safety and support,Panic attacks,Paranoia,Parenting with a mental health problem,Peer support,Personal budgets,Personal information,Personality disorders,Phobias,Physical activity, sport and exercise,Postnatal depression,Post-traumatic stress disorder,ptsd,Psychosis,Relaxation,Schizoaffective disorder,Schizophrenia,Seasonal affective disorder,Sectioning,Seeking help for a mental health problem,Self-esteem,Self-harm,Sleep problems,Sleep problems,Sleeping pills,tranquillisers,St John's wort,mental health statistics,mental health facts,Stress,Student life,Suicidal feelings,Suicide,Talking treatments,Tardive dyskinesia,Wellbeing,emergency services";
-    
-    var search_array = search_terms.split(',');
-    
-    function process_tweet(tweet) {
-        console.time('process_tweet');
-        if (search(tweet.text.toLowerCase(), search_array)) {
-            var location = ((tweet.place.country) ? tweet.place.country : tweet.user.location);
-            if (tweet.entities.hashtags) {
-                for (var hashtag in tweet.entities.hashtags) {
-                    console.log(hashtag);
-                }
-            }
-            var tweet_data = { user: tweet.user.name, text : tweet.text, location : location }
-            console.timeEnd('process_tweet');
-            return tweet_data;
-        } else return null;
-    } 
-    
-    /* Twitter stream for geolocation bounding box around UK */
-    var uk = ['-9.05', '48.77', '2.19', '58.88'];
-    
-    var stream_location = twitter.stream('statuses/filter', { /*rack: search_terms,*/locations: uk/*, language : 'en'*/ })  
-    var count_location = 0;
-    
-    stream_location.on('tweet', function(tweet) {
-        if (process_tweet(tweet)) {
-            var tweet_data = process_tweet(tweet);
-            console.log(count_location++);
-            console.log(tweet_data);
-            io.sockets.emit('twitter', tweet_data);
-        };
-    });
-    
-    stream_location.on('error', function(error) {
-        console.log(error);
-    });
-    
-    
-    
-    /* Twitter stream for keywords */
-    // var stream_keywords = twitter.stream('statuses/filter', { track: search_terms,/*locations: uk,*/ language : 'en'})  
-    // var count_keywords = 0;
-    
-    // stream_keywords.on('tweet', function(tweet) {
-    //     io.sockets.emit('twitter', tweet.text);
-    // });
-    
-    // stream_keywords.on('error', function(error) {
-    //     io.sockets.emit('twitter', error);
-    // });
+        var tweet_data = { user: tweet.user.name, text : tweet.text, location : location }
+        console.timeEnd('process_tweet');
+        return tweet_data;
+    } else return null; 
+} 
 
+function main() {
+	check_command_line_arguments();
+    change_to_file_directory();
+}
+
+main();
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+// change the keys and secrets to environment variables later for security, set using command line when node instantiated or using export
+var twitter = new Twit({
+    consumer_key : 'kirRWQYmrpOB45THZFXSICM2u',
+    consumer_secret : '1m2q5Q0tAPInrjGrvPQ1manCo27oAXWMdOcMaBFNA8iubjAini',
+    access_token : '33188875-5YWOXnLbgSRl8Tsfkjjqm2ao5uA0OuXQY8vg2tsbo',
+    access_token_secret : 'tqSIAHX6X9b8RnARVYahNtod3AWvf414dOPr57zqFx5aX'
+});
+
+twitter.get('search/tweets', 
+    {
+        q : '#mentalhealth',
+        since : 2007-01-01
+    },
+    function (twitter_err, data, response) {
+        var subset = data.statuses;
+        if (twitter_err) throw twitter_err;
+    // 	fs.writeFile('./twitter_test.json', JSON.stringify(data.statuses.slice(0,2), null, 4), function(fs_err) {
+    // 	    if (fs_err) throw fs_err;
+    //       	console.log("Saved JSON string");
+    //   	});
+      	for (var status in subset) {
+      	    var hashtags = subset[status].entities.hashtags;
+      	    for (var hashtag in hashtags) {
+      	        console.log(hashtags[hashtag].text);
+      	    }
+      	}
+    }
+);
+
+var search_terms = "mindcharity,mentalhealth,Abuse,Addiction,dependency,Advocacy,Aftercare,Anger,Antidepressants,Antidepressants,Antipsychotics,Anxiety,panic attacks,Arts therapies,Benefits,Bereavement,Bipolar disorder,Body dysmorphic disorder,Borderline personality disorder,BPD,Carers,coping,Clinical Negligence,Cognitive behavioural therapy,CBT,Community care,aftercare,mental health,social care,Complementary therapy,alternative therapy,Consent to treatment,CRHT,Crisis services,Debt and mental health,Depression,Dialectical behaviour therapy,Disability discrimination,Discharge from hospital,Discrimination at work,Dissociative disorders,Driving,Drugs - street drugs & alcohol,Eating problems,Ecotherapy,Electroconvulsive therapy,Financial help,Hearing voices,Hoarding,Holidays and respite care,Housing,Human Rights Act 1998,Hypomania and mania,IMHAs (England),IMHAs (Wales),Insurance cover and mental health,Learning disability support,LGBT mental health,Lithium and other mood stabilisers,Loneliness,Medication,Medication - drugs A-Z,Medication - stopping or coming off,Mental Capacity Act 2005,Mental Health Act 1983,Mental health and the courts,Mental health and the police,Mental health problems,Mindfulness,Money and mental health,Nearest relative,Neurosurgery for mental disorder,Obsessive-compulsive disorder,ocd,Online safety and support,Panic attacks,Paranoia,Parenting with a mental health problem,Peer support,Personal budgets,Personal information,Personality disorders,Phobias,Physical activity, sport and exercise,Postnatal depression,Post-traumatic stress disorder,ptsd,Psychosis,Relaxation,Schizoaffective disorder,Schizophrenia,Seasonal affective disorder,Sectioning,Seeking help for a mental health problem,Self-esteem,Self-harm,Sleep problems,Sleep problems,Sleeping pills,tranquillisers,St John's wort,mental health statistics,mental health facts,Stress,Student life,Suicidal feelings,Suicide,Talking treatments,Tardive dyskinesia,Wellbeing,emergency services";
+
+var search_array = search_terms.split(',');
+
+
+/* Twitter stream for geolocation bounding box around UK */
+var uk = ['-9.05', '48.77', '2.19', '58.88'];
+
+var stream_location = twitter.stream('statuses/filter', { /*rack: search_terms,*/locations: uk/*, language : 'en'*/ })  
+var count_location = 0;
+
+stream_location.on('tweet', function(tweet) {
+    if (process_tweet(tweet, search_array)) {
+        var tweet_data = process_tweet(tweet, search_array);
+        console.log(count_location++);
+        console.log(tweet_data);
+        io.sockets.emit('twitter', tweet_data);
+    };
+});
+
+stream_location.on('error', function(error) {
+    console.log(error);
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/', express.static(__dirname + '/../frontend'));
+
+io.on('connection', function(client) {
+    console.log('Client '+ client.id + ' connected.');
     
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'hello from server');
     });
     
-    app.use('/', express.static(__dirname + '/../frontend'));
+    client.on('disconnect', function(data) {
+        console.log('Client '+ client.id + ' disconnected.');
+    });
     
-    // for dev, will be removed in final
-    app.use('/datavis', express.static(__dirname + '/../DataVisualisation'));
-    
-    io.on('connection', function(client) {
-        console.log('Client '+ client.id + ' connected.');
-        
-        client.on('join', function(data) {
-            console.log(data);
-            client.emit('messages', 'hello from server');
-        });
-        
-        client.on('disconnect', function(data) {
-            console.log('Client '+ client.id + ' disconnected.');
-        });
-        
-        client.on('search', function(data) {
-            console.log('Search from client: ', data);
-            switch (data.data) {
-                case 'Google Trends':
-                    console.log('Getting data from Google');
-                    var options = {
-                        query   : data.query,
-                        date    : data.date,
-                        print   : true
+    client.on('search', function(data) {
+        console.log('Search from client: ', data);
+        switch (data.data) {
+            case 'Google Trends':
+                console.log('Getting data from Google');
+                var wget_options = {
+                    query   : data.query,
+                    date    : data.date,
+                    print   : true
+                };
+                var csv_options = {
+                    json : false,
+                    date : data.date
+                };
+                wget(wget_options, function(results) {
+                    var csv_data = extract_csv_data(results, csv_options)
+                    console.log(csv_data);
+                    var emit_data = {
+                        data : csv_data, 
+                        title : data.query,
+                        date : data.date
                     };
-                    wget(options, function(results) {
-                        var csv_data = extract_csv_data(results, { json : false })
-                        console.log(csv_data);
-                        client.emit('results', { data : csv_data, title : data.query })
-                    });
-                    break;
-                case 'Twitter':
-                    console.log('Getting data from Twitter');
-                    var response = 0; /* get twitter data*/
-                    break;
-                case 'instagram':
-                    console.log('Getting data from Instagram');
-                    var response = 0; /* get instagram data*/
-                    break;
-            }
-        })
-    })
-    server.listen(process.env.PORT || 3000);
-})();
+                    client.emit('results', emit_data);
+                });
+                break;
+            case 'Twitter':
+                console.log('Getting data from Twitter');
+                var response = 0; /* get twitter data*/
+                break;
+            case 'instagram':
+                console.log('Getting data from Instagram');
+                var response = 0; /* get instagram data*/
+                break;
+        }
+    });
+});
+
+server.listen(process.env.PORT || 3000);
